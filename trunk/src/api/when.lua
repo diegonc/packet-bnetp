332d6366e686e0d442553a97007c5f986bcfee72
dnietoc <dnietoc@e65fd9de-94d2-11de-b6d4-8762f465f3f9>
2010-08-06 22:33:44 +0000

    Positional parameters for oldwhen.

--- trunk/src/api/when.lua
+++ trunk/src/api/when.lua
@@ -1,22 +1,4 @@
---[[
---  when
---
---  Selects a block of fields from a list when it's associated condition
---  is true.
---
---  Walks though the list of pairs received as argument sequentially evaluating
---  the first element and if it was true executing the second element of the
---  pair as a block of fields.
---
---  Only one block is executed.
---
---  Table call: { {condition, block}, ... )
---    @par condition Function that returns true if the block should be
---                   used given the current state.
---    @par block     Block of fields that will be executed when condition
---                   is true.
---
---]]
+
 do
 	local template = {
 		protofield_type = "none",
@@ -31,7 +13,26 @@ do
 		end
 	end
 
-  function when (...)
+	--[[
+	--  when
+	--
+	--  Selects a block of fields from a list when it's associated condition
+	--  is true.
+	--
+	--  Walks though the list of pairs received as argument sequentially evaluating
+	--  the first element and if it was true executing the second element of the
+	--  pair as a block of fields.
+	--
+	--  Only one block is executed.
+	--
+	--  Table call: { {condition, block}, ... }
+	--    @par condition Function that returns true if the block should be
+	--                   used given the current state.
+	--    @par block     Block of fields that will be executed when condition
+	--                   is true.
+	--
+	--]]
+	function when (...)
 		local tmp = create_proto_field(template, {})
 		if (#arg == 1) and arg[1].tests then
 			tmp.tests = arg[1].tests
@@ -48,10 +49,30 @@ do
 		end
 		return tmp
 	end
+
+	--[[
+	--  oldwhen
+	--
+	--  Selects a block of fields from two alternatives acording to a condition
+	--  function result.
+	--
+	--  Only one block is executed.
+	--
+	--  Quick call: ( condition, block, otherwise )
+	--  Table call: { condition=..., block=..., otherwise=... }
+	--    @par condition Function that returns true if the block should be
+	--                   used given the current state.
+	--    @par block     Block of fields that will be executed when condition
+	--                   is true.
+	--    @par otherwise Block of fields used when condition is false.
+	--
+	--]]
 	function oldwhen (...)
-		local par = { { arg[1].condition, arg[1].block } }
-		if arg[1].otherwise then
-			par[2] = { function() return true end, arg[1].otherwise }
+		local args = make_args_table_with_positional_map(
+				{"condition", "block", "otherwise"}, unpack(arg))
+		local par = { { args.condition, args.block } }
+		if args.otherwise then
+			par[2] = { function() return true end, args.otherwise }
 		end
 		return when (unpack(par))
 	end
